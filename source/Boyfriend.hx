0d17d184df35388f4895a6e66b457ba89cee1c97
andreferrita-collab <andreferrita@gmail.com>
2026-05-24 16:06:40 -0300

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -3,22 +3,34 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.util.FlxTimer;
 
 using StringTools;
 
 class Boyfriend extends Character
 {
+	public var stunned:Bool = false;
+
+	public function new(x:Float, y:Float)
+	{
+		super(x, y);
+	}
+
 	override function update(elapsed:Float)
 	{
-		if (animation.curAnim.name.endsWith('miss')
-			&& animation.curAnim.finished
-			&& !debugMode)
+		if (animation.curAnim.name.startsWith('sing'))
 		{
-			playAnim('idle', true);
+			holdTimer += elapsed;
+		}
+		else
+			holdTimer = 0;
+
+		if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished && !debugMode)
+		{
+			playAnim('idle', true, false, 10);
 		}
 
-		if (animation.curAnim.name == 'firstDeath'
-			&& animation.curAnim.finished)
+		if (animation.curAnim.name == 'firstDeath' && animation.curAnim.finished)
 		{
 			playAnim('deathLoop');
 		}
