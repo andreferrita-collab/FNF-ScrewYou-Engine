8d61a15f0f0a1128ea1426303376ab160edfd513
andreferrita-collab <andreferrita@gmail.com>
2026-05-24 15:51:30 -0300

    Update Boyfriend.hx

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -15,22 +15,30 @@ class Boyfriend extends Character
 	{
 		super(x, y);
 
-		var tex = FlxAtlasFrames.fromSparrow(bfpath + "BOYFRIEND.png", bfpath + "BOYFRIEND.xml");
+		var tex = FlxAtlasFrames.fromSparrow(
+			bfpath + "BOYFRIEND.png",
+			bfpath + "BOYFRIEND.xml"
+		);
+
 		frames = tex;
+
 		animation.addByPrefix('idle', 'BF idle dance', 24, false);
 		animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
 		animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
 		animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
 		animation.addByPrefix('singDOWN', 'BF NOTE DOWN0', 24, false);
+
 		animation.addByPrefix('singUPmiss', 'BF NOTE UP MISS', 24, false);
 		animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
 		animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
 		animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
+
 		animation.addByPrefix('hey', 'BF HEY', 24, false);
 
 		animation.addByPrefix('firstDeath', "BF dies", 24, false);
 		animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
 		animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+
 		playAnim('idle');
 
 		antialiasing = true;
@@ -40,11 +48,14 @@ class Boyfriend extends Character
 		addOffset("singRIGHT", -38, -7);
 		addOffset("singLEFT", 12, -6);
 		addOffset("singDOWN", -10, -50);
+
 		addOffset("singUPmiss", -29, 27);
 		addOffset("singRIGHTmiss", -30, 21);
 		addOffset("singLEFTmiss", 12, 24);
 		addOffset("singDOWNmiss", -11, -19);
+
 		addOffset("hey", 7, 4);
+
 		addOffset('firstDeath', 37, 11);
 		addOffset('deathLoop', 37, 5);
 		addOffset('deathConfirm', 37, 69);
@@ -52,12 +63,15 @@ class Boyfriend extends Character
 
 	override function update(elapsed:Float)
 	{
-		if (animation.curAnim.name.endsWith('miss') && animation.curAnim.finished && !debugMode)
+		if (animation.curAnim.name.endsWith('miss')
+			&& animation.curAnim.finished
+			&& !debugMode)
 		{
-			playAnim('idle', true, false, 10);
+			playAnim('idle', true);
 		}
 
-		if (animation.curAnim.name == 'firstDeath' && animation.curAnim.finished)
+		if (animation.curAnim.name == 'firstDeath'
+			&& animation.curAnim.finished)
 		{
 			playAnim('deathLoop');
 		}
